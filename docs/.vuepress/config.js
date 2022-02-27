5d8423860127260f8d15b88bcc991e1ed00f58c7
guide <koushuangbwcx@163.com>
2022-02-27 18:05:51 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -78,7 +78,8 @@ module.exports = config({
     editLinks: true, docsDir: 'docs', seo: true,
     nav: [
       { text: "Java面试指南", icon: "java", link: "/home", },
-      { text: "官方知识星球", icon: "recommend", link: "https://www.yuque.com/docs/share/8a30ffb5-83f3-40f9-baf9-38de68b906dc", },
+      { text: "Java面试指北", icon: "recommend", link: "https://www.yuque.com/docs/share/f37fc804-bfe6-4b0d-b373-9c462188fec7"},
+      { text: "官方知识星球", icon: "recommend", link: "https://www.yuque.com/docs/share/8a30ffb5-83f3-40f9-baf9-38de68b906dc"},
       {
         text: "开发工具", icon: "Tools", link: "/tools/",
         items: [
