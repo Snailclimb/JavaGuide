b7673d12902f407807c2b84cd8b8694d37a413e2
guide <koushuangbwcx@163.com>
2022-04-04 09:08:31 +0800

    [docs add] Java 序列化详解

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -106,6 +106,7 @@ export const sidebarConfig = defineSidebarConfig({
               collapsable: true,
               children: [
                 "why-there-only-value-passing-in-java",
+                "serialization",
                 "reflection",
                 "proxy",
                 "io",
